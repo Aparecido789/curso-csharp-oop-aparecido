2de667a4d6cfa969f98accf212c3e8900cab00a1
Aparecido Francisco <aparecidofranciscosilva10@gmail.com>
2020-09-03 13:56:30 -0300

    Conceitos OOP - Utilizando métodos

---
+++ exercicio2/Metodos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            obj.apresentar("Ralf", 30);
+
+        }
+    }
+}
