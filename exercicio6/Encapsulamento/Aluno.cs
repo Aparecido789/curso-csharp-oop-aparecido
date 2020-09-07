f17ab89c7838dfd3068b319bf084946c3b5f931f
Aparecido Francisco <aparecidofranciscosilva10@gmail.com>
2020-09-07 17:55:27 -0300

    Conceitos OOP - Encapsulamento

---
+++ exercicio6/Encapsulamento/Aluno.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Encapsulamento
+{
+    public class Aluno
+    {
+        //Atributos
+        private double nota1, nota2;
+
+        //Média
+        private double media()
+        {
+            return (nota1+nota2)/2;
+        }
+
+        public void mensagem()
+        {
+            Console.WriteLine("Informe a primeira nota");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a segunda nota");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A média é "+media());
+
+        }
+    }
+}
