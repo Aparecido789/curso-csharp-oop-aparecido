c3a227b15afddc08b85685edb48711540378b7ad
Aparecido Francisco <aparecidofranciscosilva10@gmail.com>
2020-09-07 17:22:58 -0300

    Conceito OOP - Modificadores de Acesso

---
+++ exercicio5/Acesso/Produto.cs
@@ -0,0 +1,16 @@
+namespace Acesso
+{
+    class Produto
+    {
+        /*
+            public -> Atributos e métodos visiveis em qualquer classe
+            private -> Atributos e métodos visiveis apenas na classe onde são criados
+            protected -> Atributos e métodos visiveis em classes onde são criados ou herdados
+        
+        */
+
+        public string nome;
+        private double valor;
+
+    }
+}
---
+++ exercicio5/Acesso/Program.cs
@@ -0,0 +1,14 @@
+﻿using Internal;
+using System;
+
+namespace Acesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+        }
+    }
+}
