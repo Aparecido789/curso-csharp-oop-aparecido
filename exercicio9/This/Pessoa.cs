7d38b4d498bada8d53f27d13a47d884ef381ca66
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-09-24 13:40:42 -0300

    Conceitos OOP - This

---
+++ exercicio9/This/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+namespace This
+{
+    class Pessoa
+    {
+        // Atributo
+        private string nome = "Tatiana";
+
+        //Construtor
+        public Pessoa(string nome)
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(this.nome);
+        }
+    }
+}
