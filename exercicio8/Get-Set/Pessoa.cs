64700aa322b1aed13c0f9d54a9e510bf7efa8641
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-09-24 13:19:59 -0300

    Conceitos OPP - Get e Set

---
+++ exercicio8/Get-Set/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace Get_Set
+{
+    class Pessoa
+    {
+        // Atributo Nome
+        private string nome;
+
+        // Get e Set
+
+        public string Nome 
+        { 
+            get{return nome;}
+            set{nome=value;} 
+        }
+    }
+}
---
+++ exercicio8/Get-Set/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Get_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            p.Nome = "Aparecido";
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
