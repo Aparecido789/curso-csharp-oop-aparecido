b108448b4a2154211d1b673f6136be91b9ce714f
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-09-26 12:16:39 -0300

    Conceitos OOP - Metódos e Atributos estáticos

---
+++ exercicio12/Estatico/Exemplo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Estatico
+{
+    class Exemplo
+    {
+        // Atributo estático
+
+        public static string nome = "Aparecido";
+        // Método estático
+        public static void soma(int n1, int n2)
+        {
+            Console.WriteLine(n1+n2);
+        }
+    }
+}
